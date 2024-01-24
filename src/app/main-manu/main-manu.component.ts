d8c2c2421b771a71186f123614fa9deb55820406
Arnonchandee <arnon.chande@gmail.com>
2024-01-24 16:25:13 +0700

    ver.0.5

--- src/app/main-manu/main-manu.component.ts
+++ src/app/main-manu/main-manu.component.ts
@@ -5,10 +5,12 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-main-manu',
   templateUrl: './main-manu.component.html',
   styleUrls: ['./main-manu.component.css']
+
 })
 
 export class MainManuComponent {
   hidden = false;
+isdiabled: string|string[]|Set<string>|{ [klass: string]: any; }|null|undefined;
 
   toggleBadgeVisibility() {
     this.hidden = !this.hidden;
